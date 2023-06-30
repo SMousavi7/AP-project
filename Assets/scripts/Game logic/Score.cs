c47fdf8a19a45c2e56884d82fb99de5ff5c36002
F4rzin <farzin040403@gmail.com>
2023-06-30 17:07:07 +0330

    test commit

---
+++ Assets/scripts/Game logic/Score.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    [SerializeField] Text scoreText;
+    [SerializeField] Text highScoreText;
+    private static int score = 0;
+    private static int highScore = 0;
+    private static int scoreMultiplyer = 1;
+
+    private static void setScoreMultiply(int multiplyer)
+    {
+        Score.scoreMultiplyer = multiplyer;
+    }
+    public static int getScore()
+    {
+        return score;
+    }
+    public static void addScore(int score)
+    {
+        Score.score += score * scoreMultiplyer;
+    }
+ 
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreText.text = score.ToString() + " Points";
+        highScoreText.text = highScore.ToString() + " Points";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreText.text = score.ToString() + " Points";
+        highScoreText.text = highScore.ToString() + " Points";
+    }
+}
