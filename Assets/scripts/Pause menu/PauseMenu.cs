1c7b0aa4e20e117eaf7af41aa56b03ba245b0837
SMousavi7 <seyedsadramousavi77@gmail.com>
2023-07-01 12:54:55 +0330

    just retry button remain from pause menu

--- Assets/scripts/Pause menu/PauseMenu.cs
+++ Assets/scripts/Pause menu/PauseMenu.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
     public static bool GameIsPaused = false;
     public GameObject pauseMenuUI;
+    public Animator animator;
+    [SerializeField] GameObject on;
+    [SerializeField] GameObject off;
 
     // Update is called once per frame
     void Update()
@@ -24,7 +28,7 @@ public class PauseMenu : MonoBehaviour
         
     }
 
-    void resume()
+    public void resume()
     {
         pauseMenuUI.SetActive(false);
         Time.timeScale = 1f;
@@ -36,5 +40,48 @@ public class PauseMenu : MonoBehaviour
         pauseMenuUI.SetActive(true);
         Time.timeScale = 0f;
         GameIsPaused = true;
+    }
+
+    public void LoadPreviousScene()
+    {
+        StartCoroutine(ls(SceneManager.GetActiveScene().buildIndex));
+    }
+
+    IEnumerator ls(int index)
+    {
+
+        SceneManager.LoadScene(index - 1);
+        yield return new WaitForSeconds(2);
+        SceneManager.LoadScene(index);
+        animator.SetTrigger("start");
+        yield return new WaitForSeconds(2);
+        SceneManager.LoadScene(index);
+    }
+
+    public void LoadThisScene()
+    {
+        StartCoroutine(ls(SceneManager.GetActiveScene().buildIndex));
+    }
+
+    IEnumerator newls(int index)
+    {
+        SceneManager.LoadScene(index - 1);
+        yield return new WaitForSeconds(2);
+        SceneManager.LoadScene(index);
+        animator.SetTrigger("start");
+        yield return new WaitForSeconds(2);
+        SceneManager.LoadScene(index);
+    }
+
+    public void On()
+    {
+        AudioListener.volume = 1;
+        on.SetActive(true);
+    }
+
+    public void Off()
+    {
+        AudioListener.volume = 0;
+        off.SetActive(true);
     }
 }
