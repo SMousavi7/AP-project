b8e827292e7a3165936a19b44d144bdb3b945a17
SMousavi7 <seyedsadramousavi77@gmail.com>
2023-07-01 11:27:49 +0330

    just for now

--- Assets/scripts/Game logic/PlayerMovement.cs
+++ Assets/scripts/Game logic/PlayerMovement.cs
@@ -10,58 +10,58 @@ using UnityEngine.UI;
 
 public class PlayerMovement : MonoBehaviour
 {
-	[SerializeField] Rigidbody PlayerRigidbody; 
-	[SerializeField] Transform PlayerTransform;
-	[SerializeField] Bullet bullet;
-	[SerializeField] Text clock;
-	private Vector3 MAX_VELOCITY = Vector3.zero;
-	public static float fireRate = 10f;
-	public static int difficultyLevel;
-	//private float MAX_BORDER = 375f;
-	private float gunCoolDown = 0f;
-	bool invulnerable = false;
-	bool threeShot = false;
-	float multcounter = 0;
+    [SerializeField] Rigidbody PlayerRigidbody;
+    [SerializeField] Transform PlayerTransform;
+    [SerializeField] Bullet bullet;
+    [SerializeField] Text clock;
+    private Vector3 MAX_VELOCITY = Vector3.zero;
+    public static float fireRate = 10f;
+    public static int difficultyLevel;
+    //private float MAX_BORDER = 375f;
+    private float gunCoolDown = 0f;
+    bool invulnerable = false;
+    bool threeShot = false;
+    float multcounter = 0;
     // Start is called before the first frame update
     void Start()
-	{
+    {
         fireRate = 5f * Time.deltaTime;
         StreamReader sr = new StreamReader("Difficulty.txt");
-		string str = sr.ReadLine();
-		sr.Close();
-		difficultyLevel = int.Parse(str);
-		if( difficultyLevel == 3)
-		{
-			fireRate = 7f * Time.deltaTime;
-		}
-		MAX_VELOCITY.Set(75000 * Time.deltaTime, 0, 0);
-	}
+        string str = sr.ReadLine();
+        sr.Close();
+        difficultyLevel = int.Parse(str);
+        if (difficultyLevel == 3)
+        {
+            fireRate = 7f * Time.deltaTime;
+        }
+        MAX_VELOCITY.Set(75000 * Time.deltaTime, 0, 0);
+    }
 
-	public void setFireRate(float fireRate)
-	{
-		PlayerMovement.fireRate = fireRate;
-	}
+    public void setFireRate(float fireRate)
+    {
+        PlayerMovement.fireRate = fireRate;
+    }
 
-	public void setTwoShot(bool twoShot)
-	{
-		this.threeShot = twoShot;
-	}
+    public void setTwoShot(bool twoShot)
+    {
+        this.threeShot = twoShot;
+    }
 
-	public void setInvulnerable(bool able)
-	{
-		invulnerable = able;
-	}
+    public void setInvulnerable(bool able)
+    {
+        invulnerable = able;
+    }
 
 
     private void OnCollisionEnter(Collision collision)
     {
-		print(collision.gameObject.name);
-		if (collision.gameObject.CompareTag("Enemy"))
-		{
-			if (!invulnerable)
-			{
-				Destroy(this.gameObject);
-				print("game ended");
+        print(collision.gameObject.name);
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            if (!invulnerable)
+            {
+                Destroy(this.gameObject);
+                print("game ended");
 
                 IPEndPoint serverAddress = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 1234);
 
@@ -78,9 +78,9 @@ public class PlayerMovement : MonoBehaviour
                 clientSocket.Send(toSendLenBytes);
                 clientSocket.Send(toSendBytes);
 
-				StreamReader sr = new StreamReader("temp_username.txt");
-				string str = sr.ReadLine();
-				sr.Close();
+                StreamReader sr = new StreamReader("temp_username.txt");
+                string str = sr.ReadLine();
+                sr.Close();
 
                 int toSendLen1 = System.Text.Encoding.ASCII.GetByteCount(str);
                 byte[] toSendBytes1 = System.Text.Encoding.ASCII.GetBytes(str);
@@ -94,20 +94,20 @@ public class PlayerMovement : MonoBehaviour
                 byte[] rcvBytes = new byte[rcvLen];
                 clientSocket.Receive(rcvBytes);
                 String rcv = System.Text.Encoding.ASCII.GetString(rcvBytes);
-				if(rcv.Equals("new record"))
-				{
-					print(rcv);
-				}
-				else
-				{
-					print(Score.getScore());
-				}
+                if (rcv.Equals("new record"))
+                {
+                    print(rcv);
+                }
+                else
+                {
+                    print(Score.getScore());
+                }
             }
-		}
+        }
         if (collision.gameObject.name.StartsWith("threeshot"))
         {
-			setTwoShot(true);
-			multcounter = -5f;
+            setTwoShot(true);
+            multcounter = -5f;
         }
         if (collision.gameObject.name.StartsWith("shield"))
         {
@@ -116,116 +116,116 @@ public class PlayerMovement : MonoBehaviour
         }
         if (collision.gameObject.name.StartsWith("invulnerbility"))
         {
-			print("invulnerbility");
-			setInvulnerable(true);
-			multcounter = -5f;
+            print("invulnerbility");
+            setInvulnerable(true);
+            multcounter = -5f;
         }
         if (collision.gameObject.name.StartsWith("bomb"))
         {
-			BallMovement.bombed = true;
-			multcounter = -5f;
-			print("bomb");
+            BallMovement.bombed = true;
+            multcounter = -5f;
+            print("bomb");
         }
         if (collision.gameObject.name.StartsWith("timestop"))
         {
-			BallMovement.timestop = true;
-			multcounter = -5f;
-			print("timestop");
+            BallMovement.timestop = true;
+            multcounter = -5f;
+            print("timestop");
         }
         if (collision.gameObject.name.StartsWith("multiply"))
         {
-			print("mult");
-			Score.setMult(2);
-			multcounter = -5f;
+            print("mult");
+            Score.setMult(2);
+            multcounter = -5f;
         }
         if (collision.gameObject.name.StartsWith("firerate"))
         {
-			print("firerate");
-			setFireRate(2f * Time.deltaTime);
+            print("firerate");
+            setFireRate(2f * Time.deltaTime);
             multcounter = -5f;
         }
     }
 
     // Update is called once per frame
     void FixedUpdate()
-	{
-		
-		if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
-		{
-			if(PlayerRigidbody.velocity.x > -MAX_VELOCITY.x)
-			{ 
-				if(PlayerRigidbody.velocity.x > 30000 * Time.deltaTime)
-				{
+    {
+
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        {
+            if (PlayerRigidbody.velocity.x > -MAX_VELOCITY.x)
+            {
+                if (PlayerRigidbody.velocity.x > 30000 * Time.deltaTime)
+                {
                     PlayerRigidbody.AddForce(-100000 * (PlayerRigidbody.velocity.x / 150.0f) * Time.deltaTime, 0, 0);
                 }
                 else
-				{	
-					PlayerRigidbody.AddForce(-50000 * Time.deltaTime, 0, 0);
-				}
-			}
-		}
-		else if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
-		{
-			if (PlayerRigidbody.velocity.x < MAX_VELOCITY.x)
-			{
-				if (PlayerRigidbody.velocity.x < -30000 * Time.deltaTime)
-				{
-					PlayerRigidbody.AddForce(100000 * (PlayerRigidbody.velocity.x / -150.0f) * Time.deltaTime, 0, 0);
-				}
-				else
-				{
-					PlayerRigidbody.AddForce(50000 * Time.deltaTime, 0, 0);
+                {
+                    PlayerRigidbody.AddForce(-50000 * Time.deltaTime, 0, 0);
                 }
             }
-		}
-		else
-		{
-			PlayerRigidbody.velocity = PlayerRigidbody.velocity * 1f * Time.deltaTime;
-		}
-		PlayerRigidbody.velocity.Normalize();
-		if (Input.GetKey(KeyCode.Space) || Input.GetMouseButton((int)MouseButton.Left))
-		{
-			if(gunCoolDown >= 0)
-			{
-				Vector3 pos = Vector3.zero;
-				Quaternion rot = Quaternion.identity;
-				pos.Set(PlayerTransform.position.x, -156f, -83f);
-				Instantiate(bullet, pos, rot);
-				if (threeShot)
-				{
-					pos.Set(PlayerTransform.position.x + 20f, -210f, -83f);
-					Instantiate(bullet, pos, rot);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        {
+            if (PlayerRigidbody.velocity.x < MAX_VELOCITY.x)
+            {
+                if (PlayerRigidbody.velocity.x < -30000 * Time.deltaTime)
+                {
+                    PlayerRigidbody.AddForce(100000 * (PlayerRigidbody.velocity.x / -150.0f) * Time.deltaTime, 0, 0);
+                }
+                else
+                {
+                    PlayerRigidbody.AddForce(50000 * Time.deltaTime, 0, 0);
+                }
+            }
+        }
+        else
+        {
+            PlayerRigidbody.velocity = PlayerRigidbody.velocity * 1f * Time.deltaTime;
+        }
+        PlayerRigidbody.velocity.Normalize();
+        if (Input.GetKey(KeyCode.Space) || Input.GetMouseButton((int)MouseButton.Left))
+        {
+            if (gunCoolDown >= 0)
+            {
+                Vector3 pos = Vector3.zero;
+                Quaternion rot = Quaternion.identity;
+                pos.Set(PlayerTransform.position.x, -156f, -83f);
+                Instantiate(bullet, pos, rot);
+                if (threeShot)
+                {
+                    pos.Set(PlayerTransform.position.x + 20f, -210f, -83f);
+                    Instantiate(bullet, pos, rot);
                     pos.Set(PlayerTransform.position.x - 20f, -210f, -83f);
                     Instantiate(bullet, pos, rot);
                 }
-				gunCoolDown = -fireRate;
-			}
-		}
-		if(gunCoolDown < 0)
-		{
-			gunCoolDown += Time.deltaTime;
-		}
-		clock.text = " ";
-		if(multcounter < 0)
-		{
-			clock.text = "Power up remaining: " + ((int)-multcounter).ToString();
-			multcounter += Time.deltaTime;
-			if(multcounter >= 0) 
-			{
-				Score.setMult(1);
-				if(difficultyLevel == 3)
-				{
-					setFireRate(7f * Time.deltaTime);
-				}
-				else
-				{
-					setFireRate(5f * Time.deltaTime);
-				}
-				setInvulnerable(false);
-				BallMovement.timestop = false;
-				BallMovement.bombed = false;
-				setTwoShot(false);
-			}
-		}
+                gunCoolDown = -fireRate;
+            }
+        }
+        if (gunCoolDown < 0)
+        {
+            gunCoolDown += Time.deltaTime;
+        }
+        clock.text = " ";
+        if (multcounter < 0)
+        {
+            clock.text = "Power up remaining: " + ((int)-multcounter).ToString();
+            multcounter += Time.deltaTime;
+            if (multcounter >= 0)
+            {
+                Score.setMult(1);
+                if (difficultyLevel == 3)
+                {
+                    setFireRate(7f * Time.deltaTime);
+                }
+                else
+                {
+                    setFireRate(5f * Time.deltaTime);
+                }
+                setInvulnerable(false);
+                BallMovement.timestop = false;
+                BallMovement.bombed = false;
+                setTwoShot(false);
+            }
+        }
     }
 }
